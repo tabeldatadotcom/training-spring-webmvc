c9310ed5e71f9667959e9f235ccd205302345643
dimasm93 <software.dimas_m@icloud.com>
2023-12-13 19:46:13 +0700

    http request web

--- src/main/java/com/maryanto/dimas/training/bni/controller/IndexController.java
+++ src/main/java/com/maryanto/dimas/training/bni/controller/IndexController.java
@@ -1,13 +1,23 @@
 package com.maryanto.dimas.training.bni.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/app/index")
-    public String index() {
+    @GetMapping("/app/index.php")
+    public String index(
+            @RequestParam("namaLengkap") String namaLengkap,
+            Model model) {
+        model.addAttribute("namaLengkap", namaLengkap);
+        return "index";
+    }
+
+    @GetMapping("/app/index.java")
+    public String index2() {
         return "index";
     }
 }
